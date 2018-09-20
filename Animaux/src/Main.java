c1c58a8b225bc379ed29cf0831d6cf9600c7ae5f
59013-64-03 <59013-64-03@STA6100901.exchange.ad.afpanet>
2018-09-20 10:58:57 +0200

    add

--- Animaux/src/Main.java
+++ Animaux/src/Main.java
@@ -2,8 +2,28 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Début du programme : ");
+		Chien popy = new Chien();
+		popy.nom = "Popy";
+		popy.taille= 5;
+		popy.poids = 15;
+		popy.couleur = "noir";
+		popy.race = "bull";
+	System.out.println("------------------------------------");
+		
+		System.out.println("Nom : " + popy.nom);
+		System.out.println("taille : " + popy.taille);
+		System.out.println("poids : " + popy.poids);
+		System.out.println("couleur : " + popy.couleur);
+		System.out.println("race : " + popy.race);
+		
+		popy.manger();
+		popy.dormir ();
+		popy.jouer();
+		popy.aboyer();
+		popy.mordre();
+		
+				
 	}
 
 }
